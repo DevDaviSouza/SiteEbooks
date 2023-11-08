af52ef5263047eda842cece395890bf36bfb3708
Davi Souza Santos <davii.clink@gmail.com>
2023-11-08 00:40:31 -0300

    criação inicial do site

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,20 +7,16 @@ export default function Home() {
   return (
     <body className=' justify-center items-center flex '>
       <main className='w-4/5 border bg-gray-800 m-7'>
-        <nav className=' flex row justify-between items-center'>  
-              <Image
-                      src="/fotinha.jpg"
-                      alt="Vercel Logo"
-                      className="logo "
-                      width={100}
-                      height={24}
-              />
+        <nav className=' flex row justify-between items-center p-4'>  
+         
+          
+          <img src='https://icons.iconarchive.com/icons/martz90/circle/32/books-icon.png' alt='teste' width={80}/>
               
-              <div className='menu flex gap-32'>
-                <div>Home</div>
-                <div>WEB</div>
-                <div>Mobile</div>
-                <div>Languages</div>
+              <div className='menu flex gap-32 text-2xl'>
+                <div className='cursor-pointer'>Home</div>
+                <div className='cursor-pointer'>WEB</div>
+                <div className='cursor-pointer'>Mobile</div>
+                <div className='cursor-pointer'>Desktop</div>
               </div>
 
               <div className='user pl-20'>
@@ -35,12 +31,88 @@ export default function Home() {
               </div>
         </nav>
       
-        <section className='bg-gray-500 items-center justify-center flex '>
-          <h1 className='text-3xl pt-12 pb-12'>
+        <h1 className='text-3xl pt-7 pb-7 text-center bg-gray-500'>
             Baixe os Ebooks que quiser de forma totalmente gratuita!
           </h1>
-
+        <section className=' items-center justify-center flex  flex-col'>
           
+
+          <div className='pt-10 cards columns-3 gap-y-7 gap-x-32 bg-gray-800 '>
+                 <Image
+                    src="/capa.jpg"
+                    alt="user"
+                    className='mb-10'
+                    width={250}
+                    height={500}
+                  />
+                  
+                  <Image
+                          src="/capa.jpg"
+                          alt="user"
+                          className='mb-10'
+                          width={250}
+                          height={500}
+                  />
+                  
+                  <Image
+                          src="/capa.jpg"
+                          alt="user"
+                          className='mb-10'
+                          width={250}
+                          height={500}
+                  />
+                  
+                  <Image
+                          src="/capa.jpg"
+                          alt="user"
+                          className='mb-10'
+                          width={250}
+                          height={500}
+                  />
+                  
+                  <Image
+                          src="/capa.jpg"
+                          alt="user"
+                          className='mb-10'
+                          width={250}
+                          height={500}
+                  />
+                  
+                  <Image
+                          src="/capa.jpg"
+                          alt="user"
+                          className='mb-10'
+                          width={250}
+                          height={500}
+                  />
+                  
+                  <Image
+                          src="/capa.jpg"
+                          alt="user"
+                          className='mb-10'
+                          width={250}
+                          height={500}
+                  />
+                  
+                  <Image
+                          src="/capa.jpg"
+                          alt="user"
+                          className='mb-10'
+                          width={250}
+                          height={500}
+                  />
+                  
+                  <Image
+                          src="/capa.jpg"
+                          alt="user"
+                          className='mb-10'
+                          width={250}
+                          height={500}
+                  />
+            
+                
+
+          </div>
         </section>
 
       </main>
